c5e49ded960e91c565386c95f387e6be37fd2807
bigshowlion <click1503@gmail.com>
2021-08-10 22:45:25 +0700

    First version

---
+++ API.py
@@ -0,0 +1,8 @@
+import requests
+
+api_key = "r43nld0gf1c7xlmtw2xygontzimy60f"
+product_id = 99688
+
+r = requests.get(https://api.bigcommerce.com/stores/q6xsfp1zsy/v3/catalog/products/product_id/options)
+
+print(r)
